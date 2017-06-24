5c23c0a3c2171293cbf6de737bfa69680d172286
jfedoruk <kubafedoruk@gmail.com>
2017-06-24 11:23:07 +0200

    Add a display method for Vending Machine.
    Extend testVendingMachine to check for correct output on stdout.

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -2,9 +2,16 @@ package tdd.vendingMachine;
 
 public class VendingMachine {
 
+    private String separator = "######## Vending Machine ########";
     private double money = 0;
 
     public double getMoney() {
         return money;
+    }
+
+    public void display() {
+        System.out.println(separator);
+        System.out.println("Money: " + getMoney());
+        System.out.println(separator);
     }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,16 +1,36 @@
 package tdd.vendingMachine;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
 
     @Test
     public void testVendingMachine() {
         VendingMachine vendingMachine = new VendingMachine();
 
         assertEquals(vendingMachine.getMoney(), 0, 0.0);
+        vendingMachine.display();
+        assertThat(outContent.toString()).contains("Money: 0.0");
     }
 
 }
