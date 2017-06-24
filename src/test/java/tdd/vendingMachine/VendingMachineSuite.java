e83f5286f05aa6fdc6ccaf62978003f19256ff24
jfedoruk <kubafedoruk@gmail.com>
2017-06-24 14:00:35 +0200

    Add a suite to easily run all tests.

---
+++ src/test/java/tdd/vendingMachine/VendingMachineSuite.java
@@ -0,0 +1,13 @@
+package tdd.vendingMachine;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    VendingMachineTest.class,
+    VendingMachineProductTest.class
+})
+public class VendingMachineSuite {
+
+}
